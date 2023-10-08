52a64593799d92774a4a39ded50280f8931991d8
Sabir Aboobaker <sabiraboobacker007@gmail.com>
2023-10-08 06:54:57 +0530

    initiated project

---
+++ Section3/initializeDB.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import User, { IUser } from './models/Users'; // Import your User model
+import bcrypt from 'bcrypt'; 
+
+// Connect to your MongoDB database
+mongoose.connect('mongodb://localhost/your-database-name', {
+    dbName:"MTCT"
+})
+  .then(async () => {
+    console.log('Connected to MongoDB');
+    const salt = await bcrypt.genSalt(10);
+
+    // Define the JSON data to insert
+    const userData: Partial<IUser>[] = [
+      {
+        userId: 'MTCT001',
+        name: 'Head Quarters',
+        type: 'hq',
+        password: await bcrypt.hash('hq123', salt),
+      },
+      {
+        userId: 'MTCT002',
+        name: 'Branch 1',
+        type: 'branch',
+        password: await bcrypt.hash('br123', salt),
+      },
+      {
+        userId: 'MTCT003',
+        name: 'Branch 2',
+        type: 'branch',
+        password: await bcrypt.hash('br123', salt),
+      },
+      {
+        userId: 'MTCT004',
+        name: 'Advisor 1',
+        type: 'advisor',
+        password: await bcrypt.hash('fa123', salt),
+      },
+      {
+        userId: 'MTCT005',
+        name: 'Advisor 2',
+        type: 'advisor',
+        password: await bcrypt.hash('fa123', salt),
+      },
+    ];
+
+    // Insert the data into the User collection
+    User.insertMany(userData as IUser[])
+      .then(() => {
+        console.log('Data inserted successfully');
+      })
+      .catch((error) => {
+        console.error('Error inserting data:', error);
+      })
+      .finally(() => {
+        // Close the database connection
+        mongoose.disconnect();
+      });
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
